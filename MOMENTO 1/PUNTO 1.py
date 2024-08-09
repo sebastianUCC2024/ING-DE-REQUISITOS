6db9c2dcb8fd32d0b0f446092728997f141a5c4e
sebastianUCC2024 <juans.patino@campussucc.edu.co>
2024-08-09 16:02:51 -0500

    first commit

---
+++ MOMENTO 1/PUNTO 1.py
@@ -0,0 +1,24 @@
+# punto 1
+print("  Base del rectangulo ")
+baseRectangulo=input(" Ingrese la longitud de la base : ")
+
+print(" Altura del rectangulo ")
+alturaRectangulo=input(" Ingrese la altura : ")
+
+print("Area del rectangulo")
+areaRenctangulo=(int(baseRectangulo)*int(alturaRectangulo))
+
+print("  Base del triangulo ")
+baseTriangulo=input(" Ingrese la longitud de la base : ")
+
+print(" Altura del triangulo ")
+alturaTriangulo=input(" Ingrese la altura : ")
+
+print("Area del rectangulo")
+areaTriangulo=(int(baseTriangulo)*int(alturaRectangulo)/2)
+
+
+print("Area del terreno")
+Area=(areaTriangulo+areaRenctangulo)
+print(Area)
+
---
+++ MOMENTO 1/PUNTO 2.py
@@ -0,0 +1,7 @@
+print("Cantidad de leche produciada en litros")
+cantidadLeche = input("ingrese la cantidad de leche producida: ")
+
+print("Conversion de litros a galones")
+conversion = (int(cantidadLeche)*float(3.785))
+
+print(f'la cantidad de leche producida en galones {conversion}')
