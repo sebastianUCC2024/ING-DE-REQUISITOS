261f9f998ba694b4cb0ba31cc2a4633befe1216a
sebastianUCC2024 <juans.patino@campussucc.edu.co>
2024-08-14 21:19:04 -0500

    ##

--- MOMENTO 1/PUNTO 1.py
+++ MOMENTO 1/PUNTO 1.py
@@ -1,24 +1,20 @@
-# punto 1
-print("  Base del rectangulo ")
-baseRectangulo=input(" Ingrese la longitud de la base : ")
 
-print(" Altura del rectangulo ")
-alturaRectangulo=input(" Ingrese la altura : ")
-
-#AREA DEL RECTANGULO
-areaRenctangulo=(int(baseRectangulo)*int(alturaRectangulo))
-
-print("  Base del triangulo ")
-baseTriangulo=input(" Ingrese la longitud de la base : ")
-
-print(" Altura del triangulo ")
-alturaTriangulo=input(" Ingrese la altura : ")
-
-#AREA DEL TRIANGULO
-areaTriangulo=(int(baseTriangulo)*int(alturaRectangulo)/2)
+print("AREA DEL RECTANGULO ")
+baseRectangulo = int(input(" Ingrese la longitud de la base : "))
+alturaRectangulo = int(input(" Ingrese la altura : "))
+areaRenctangulo = baseRectangulo * alturaRectangulo
+print(f'La area del rectangulo es de {areaRenctangulo} metros')
 
 
-print("Area del terreno")
-Area=(areaTriangulo+areaRenctangulo)
-print(Area)
+print("AREA DEL TRIANGULO")
+baseTriangulo = int(input(" Ingrese la longitud de la base : "))
+alturaTriangulo = int(input(" Ingrese la altura : "))
+areaTriangulo = baseTriangulo * alturaRectangulo/2
+print(f'La area del triangulo es de {areaTriangulo} metros')
+
+
+
+print("AREA DEL TERRENO")
+Area = areaTriangulo + areaRenctangulo
+print(f'La area del terreno es de {Area} metros')
 
